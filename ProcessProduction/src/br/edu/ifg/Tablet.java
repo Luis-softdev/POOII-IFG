c654a31d3a712588141f2ef2784ceb29a79fcdf6
Pedrogabriel152 <105892538+Pedrogabriel152@users.noreply.github.com>
2023-03-11 17:55:51 -0300

    Final

--- ProcessProduction/src/br/edu/ifg/Tablet.java
+++ ProcessProduction/src/br/edu/ifg/Tablet.java
@@ -1,24 +1,59 @@
 package br.edu.ifg;
 import java.util.Scanner;
 
-import br.edu.ifg.interfaces.tabletInter;
+import br.edu.ifg.interfaces.tabletInterface;
 
-public class Tablet implements tabletInter{
-    private String preco;
+public class Tablet implements tabletInterface{
+    private Double preco;
+    private String marca;
+    private String modelo;
+    private String nomePeca;
+
+    public Tablet(String nomePeca, String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.nomePeca = nomePeca;
+    }
 
     public void definirPreco(){
-        System.out.println("Digite qual produto será produzido: ");
+        System.out.println("\nDigite qual o valor do Tablet: ");
         Scanner scanner = new Scanner(System.in);
-        String resposta = scanner.nextLine();
-        this.preco = resposta;
+        Double resposta = scanner.nextDouble();
+        this.setPreco(resposta);
         scanner.close();
     }
 
-    public String getPreco() {
+    public Double getPreco() {
         return preco;
     }
 
-    public void setPreco(String preco) {
+    public void setPreco(Double preco) {
         this.preco = preco;
     }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getNomePeca() {
+        return nomePeca;
+    }
+
+    public void setNomePeca(String nomePeca) {
+        this.nomePeca = nomePeca;
+    }
+
+    
 }
