91612c9afa55b25e28935b528d936d663d391d06
luisfernand0dev <luisferando.honorato@gmail.com>
2023-03-11 16:26:47 -0300

    Classes - App, Tablet e SmartPhone

--- ProcessProduction/src/br/edu/ifg/smartphone.java
+++ ProcessProduction/src/br/edu/ifg/smartphone.java
@@ -1,24 +1,6 @@
 package br.edu.ifg;
-import java.util.Scanner;
 
-import br.edu.ifg.interfaces.smartphoneInter;
-
-public class smartphone implements smartphoneInter{
-    private String preco;
-
-    public void definirPreco(){
-        System.out.println("Digite o valor do produto final: ");
-        Scanner scanner = new Scanner(System.in);
-        String resposta = scanner.nextLine();
-        this.preco = resposta;
-        scanner.close();
-    }
-
-    public String getPreco() {
-        return preco;
-    }
-
-    public void setPreco(String preco) {
-        this.preco = preco;
-    }
+public class Smartphone {
+    
+    
 }
