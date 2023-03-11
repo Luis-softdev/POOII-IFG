75b78071ba95a0bac529f3e257167bd2147f1985
luisfernand0dev <luisferando.honorato@gmail.com>
2023-03-11 16:41:07 -0300

    Classes - App, Tablet e SmartPhone

--- ProcessProduction/src/br/edu/ifg/smartphone.java
+++ ProcessProduction/src/br/edu/ifg/smartphone.java
@@ -1,6 +1,25 @@
 package br.edu.ifg;
+import java.util.Scanner;
 
-public class Smartphone {
-    
+import br.edu.ifg.interfaces.smartphoneInter;
+
+public class Smartphone implements smartphoneInter {
+    private String preco;
+
+    public void definirPreco(){
+        System.out.println("Digite qual produto será produzido: ");
+        Scanner scanner = new Scanner(System.in);
+        String resposta = scanner.nextLine();
+        this.preco = resposta;
+        scanner.close();
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
     
 }
--- ProcessProduction/src/br/edu/ifg/tablet.java
+++ ProcessProduction/src/br/edu/ifg/tablet.java
@@ -1,5 +1,24 @@
 package br.edu.ifg;
+import java.util.Scanner;
 
-public class Tablet {
-    
+import br.edu.ifg.interfaces.tabletInter;
+
+public class Tablet implements tabletInter{
+    private String preco;
+
+    public void definirPreco(){
+        System.out.println("Digite qual produto será produzido: ");
+        Scanner scanner = new Scanner(System.in);
+        String resposta = scanner.nextLine();
+        this.preco = resposta;
+        scanner.close();
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
 }
